29e103a37d1b1616966532adf039bd41d0c28938
D3fau4 <raulwii2@gmail.com>
2022-01-13 08:42:11 +0100

    Small setup

--- Watson.Lib/Class1.cs
+++ Watson.Lib/Class1.cs
@@ -1,7 +1,26 @@
-﻿namespace Watson.Lib
+﻿using AssetsTools.NET.Extra;
+
+namespace Watson.Lib
 {
     public class Class1
     {
+        public void meme()
+        {
+            var am = new AssetsManager();
+            var inst = am.LoadAssetsFile("resources.assets", true);
 
+            am.LoadClassPackage("classdata.tpk");
+            am.LoadClassDatabaseFromPackage(inst.file.typeTree.unityVersion);
+
+            foreach (var inf in inst.table.GetAssetsOfType((int)AssetClassID.GameObject))
+            {
+                var baseField = am.GetTypeInstance(inst, inf).GetBaseField();
+
+                var name = baseField.Get("m_Name").GetValue().AsString();
+                Console.WriteLine(name);
+            }
+
+            am.UnloadAllAssetsFiles();
+        }
     }
 }
